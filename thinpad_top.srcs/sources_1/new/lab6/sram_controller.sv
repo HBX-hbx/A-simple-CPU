4e80f7e9a95377725cfcb49efbfc995a44b28d94
ChengQ <qianc20@mails.tsinghua.edu.cn>
2022-11-15 10:37:07 +0800

    Init Commit

--- thinpad_top.srcs/sources_1/new/lab6/sram_controller.sv
+++ thinpad_top.srcs/sources_1/new/lab6/sram_controller.sv
@@ -130,7 +130,6 @@ module sram_controller #(
                     wb_ack_o <= 1'b0;
                     state <= STATE_IDLE;
                 end
-                
             endcase
         end
     end
