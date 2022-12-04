391015cc35a1b57f067459018b08034adc31fd3b
teburile <1667612243@qq.com>
2022-12-04 10:10:31 +0800

    Fix bugs in fencei

--- thinpad_top.srcs/sources_1/new/lab6/cache/im_fast_cache.sv
+++ thinpad_top.srcs/sources_1/new/lab6/cache/im_fast_cache.sv
@@ -38,11 +38,13 @@ module im_fast_cache(
     reg [31:0] last_w_pc = 32'b0;
     reg [31:0] last_im_inst = 32'b0;
     reg if_same;
+    reg fence_start_and_finish;
+    reg fence_requested;
 
     assign if_same = (last_w_pc == pc_i) && (last_im_inst == im_inst_i);
 
     always_ff @(posedge clk_i) begin
-        if (rst_i || fence_i) begin // init with all cache fake
+        if (rst_i) begin // init with all cache fake
             j = 6'b0;
             repeat(64) begin
                 valid_1[j] <= 1'b0;
@@ -52,7 +54,30 @@ module im_fast_cache(
                 valid_2[k] <= 1'b0;
                 k = k + 1;
             end
+            fence_start_and_finish <= 0;
+            fence_requested <= 0;
+        end else if (fence_i) begin
+            if (fence_start_and_finish && im_ack_i) begin
+                // fence_start_and_finish <= 0;
+            end else if(fence_start_and_finish) begin
+                // Do nothing and wait
+                fence_requested <= 1;
+            end begin
+                j = 6'b0;
+                repeat(64) begin
+                    valid_1[j] <= 1'b0;
+                    j = j + 1;
+                end
+                repeat(64) begin
+                    valid_2[k] <= 1'b0;
+                    k = k + 1;
+                end
+                fence_start_and_finish <= 1;
+            end
+
         end else begin
+            fence_start_and_finish <= 0;
+            fence_requested <= 0;
             if (im_ack_i && ~if_same && ~cache_hit) begin
                 last_w_pc <= pc_i;
                 last_im_inst <= im_inst_i;
@@ -92,6 +117,16 @@ module im_fast_cache(
             inst_o = data_2[pc_i[7:2]];
             ack_o = 1;
             cache_hit = 1;
+        end else if (fence_i) begin
+            if (fence_start_and_finish && im_ack_i) begin
+                inst_o = im_inst_i;
+                ack_o = 1;
+                cache_hit = 0;
+            end else begin
+                inst_o = im_inst_i;
+                cache_hit = 0;
+                ack_o = 0;
+            end
         end else begin
             inst_o = im_inst_i;
             cache_hit = 0;
@@ -102,8 +137,13 @@ module im_fast_cache(
 
     always_comb begin
         if (fence_i) begin
-            im_req_o = 0;
-            im_pc_o = pc_i;
+            if (fence_start_and_finish) begin
+                im_req_o = 0;
+                im_pc_o = pc_i;
+            end else begin
+                im_req_o = 1;
+                im_pc_o = pc_i;
+            end
         end else begin
             if ((req_i == 1 || hit_longer) && (pc_tag_1[pc_i[7:2]] == pc_i[31:8]) && valid_1[pc_i[7:2]]) begin
                 // not to request wishbone
