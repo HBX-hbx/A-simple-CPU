eacad184986d189251a3f49af1e2482e900e7391
teburile <1667612243@qq.com>
2022-12-05 12:37:07 +0800

    Modify cachable addr

--- thinpad_top.srcs/sources_1/new/lab6/cache/cachable_mux.sv
+++ thinpad_top.srcs/sources_1/new/lab6/cache/cachable_mux.sv
@@ -5,18 +5,23 @@ module cachable_mux(
     // four bytes align
     // only used in dm_cache
     // TODO
+    `define CACHE_ON 1'b1;
     always_comb begin
         if ((addr >= 32'h8010_0000) && (addr <= 32'h8010_0FFF)) begin
-            cachable = 1;
+            cachable = `CACHE_ON;
             // cachable = 0;
         end else if ((addr >= 32'h0000_0000) && (addr <= 32'h002F_FFFF)) begin
-            cachable = 1;
+            cachable = `CACHE_ON;
         end else if ((addr >= 32'h7FC1_0000) && (addr <= 32'h7FFF_FFFF)) begin
-            cachable = 1;
+            cachable = `CACHE_ON;
         end else if ((addr >= 32'h8000_0000) && (addr <= 32'h8000_0FFF)) begin
-            cachable = 1;
+            cachable = `CACHE_ON;
         end else if ((addr >= 32'h8000_1000) && (addr <= 32'h8000_1FFF)) begin
-            cachable = 1;
+            cachable = `CACHE_ON;
+        end else if ((addr >= 32'h8000_0000) && (addr <= 32'h800F_FFFF)) begin
+            cachable = `CACHE_ON;
+        end else if ((addr >= 32'h807F_0000) && (addr <= 32'h807F_FFFF)) begin
+            cachable = `CACHE_ON;
         end else begin
             cachable = 0;
         end
